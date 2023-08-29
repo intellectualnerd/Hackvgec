7d53bdddc333b0f7559ce59dbf110979989a9199
unknown <parmarparth954@gmail.com>
2023-08-29 14:52:24 +0530

    hi

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName('carousel-control-prev')[0].click();
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active2");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
